8c36e23fd4f63d3dcbe65b798938fa8c5dbc4a3a
samover <samuel.overloop@gmail.com>
2015-11-25 16:02:04 +0000

    user cannot review restaurant when not logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
 
   has_many :reviews
+  has_many :reviewed_restaurants, through: :reviews, source: :restaurant
+
+  #def has_reviewed? restaurant
+    #reviewed_restaurants.include? restaurant
+  #end 
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- spec/features/reviewing_features_spec.rb
+++ spec/features/reviewing_features_spec.rb
@@ -1,15 +1,35 @@
 require 'spec_helper'
 
 feature 'Reviewing' do
-  before { Restaurant.create name: 'KFC' }
+  before { Restaurant.create name: 'Bocado' }
 
-  scenario 'Allows users to review a restaurant using a form' do
-    visit '/restaurants'
-    click_link 'Review KFC'
-    fill_in 'Thoughts', with: 'Okay'
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
-    expect(current_path).to eq '/restaurants'
-    expect(page).to have_content 'Okay'
+  context 'User logged in' do
+    scenario 'Allow users to leave a review using a form' do
+      login
+      visit '/restaurants'
+      click_link 'Review Bocado'
+      fill_in 'Thoughts', with: 'Okay'
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
+
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content 'Okay'
+    end
+
+    scenario 'User cannot review a restaurant it has already reviewed' do
+      login
+      visit '/restaurants'
+      click_link 'Review Bocado'
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content 'You already reviewed this restaurant'
+    end
+  end
+
+  context 'User not logged in' do
+    scenario 'Cannot review a restaurant' do
+      visit '/restaurants'
+      click_link 'Review Bocado'
+      expect(current_path).to eq '/users/sign_in'
+    end
   end
 end
